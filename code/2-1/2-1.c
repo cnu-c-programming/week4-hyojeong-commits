7cf05324cee10de3e7108ffb858764cca467491f
hyojeong-commits <kweunsungo@gmail.com>
2026-04-02 22:50:21 +0900

    Update 2-1.c

--- code/2-1/2-1.c
+++ code/2-1/2-1.c
@@ -6,7 +6,7 @@ int main() {
     int year, month, day;
 
     sscanf(date, "%d-%d-%d", &year, &month, &day);
-    sprintf(buffer, "%02d/%02d/%02d", month, day, year % 100);
+    sprintf(buffer, "%02d/%d/%d", month, day, year % 100);
 
     printf("%s\n", buffer);  
     return 0;
