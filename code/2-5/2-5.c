b1486c15eb8f5c49d63f2ca3978f6c5106028acb
hyojeong-commits <kweunsungo@gmail.com>
2026-04-02 22:42:42 +0900

    Update 2-5.c

--- code/2-5/2-5.c
+++ code/2-5/2-5.c
@@ -6,7 +6,7 @@ int main() {
     double y = 2.5;
 
     printf("%.2f\n", sqrt(x));      
-    printf("%.2f\n", pow(y, 5));    
+    printf("%.2f\n", pow(x,y));    
     printf("%.1f\n", ceil(y));      
     printf("%.1f\n", floor(y)); 
   
