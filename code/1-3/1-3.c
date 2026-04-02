ca4c88011533d3091a2d20028858df19ab734d49
hyojeong-commits <kweunsungo@gmail.com>
2026-04-02 22:56:33 +0900

    Update 1-3.c

--- code/1-3/1-3.c
+++ code/1-3/1-3.c
@@ -6,4 +6,22 @@ extern void task2();
 extern void logger_global(char*message);
 
 void task1() {
-  for(int i=0; i
+  for(int i=0; i<5; i++) {
+    logger_static_inline("task1");
+    logger_global("task1");
+    }
+}
+
+void task3() {
+  for(int i=0; i<3; i++) {
+    logger_static_inline("task3");
+    logger_global("task3");
+    }
+}
+
+int main() {
+    task1();
+    task2();
+    task3();
+    return 0;
+}
