b6d57739e8738b690e8132cf3cdcfd653c73a35f
hyojeong-commits <kweunsungo@gmail.com>
2026-04-02 21:57:28 +0900

    Update 3-1.c

---
+++ code/3-1/3-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10;
+    int* p = &n;
+
+    printf("address of n: %p\n", (void*)&n);
+    printf("*p = %d\n", *p);
+    *p = 99;
+    printf("n = %d\n", n);   
+    return 0;
+}
