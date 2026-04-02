138cba07cedc7db1ca02466ddc0808e132907ca6
hyojeong-commits <kweunsungo@gmail.com>
2026-04-02 21:46:56 +0900

    Update 2-1.c

---
+++ code/2-1/2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    const char* date = "2026-03-24";
+    char buffer[64];
+    int year, month, day;
+
+    sscanf(date, "%d-%d-%d", &year, &month, &day);
+    sprintf(buffer, "%02d/%02d/%02d", month, day, year % 100);
+
+    printf("%s\n", buffer);  // 03/24/26
+    return 0;
+}
