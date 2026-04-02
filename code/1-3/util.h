9b3acb9d75f91654ebe7ef930805c2a1b6811acb
hyojeong-commits <kweunsungo@gmail.com>
2026-04-02 23:04:33 +0900

    Update util.h

--- code/1-3/util.h
+++ code/1-3/util.h
@@ -2,5 +2,5 @@
 
 static inline void logger_static_inline(char*message) {
   static int counter = 0;
-  printf("log &2d: %s\n", counter++, messaage);
+  printf("log &2d: %s\n", counter++, message);
 }
